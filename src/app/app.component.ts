f683d9ab48be1ff3e636b296887a6abf228d8e7d
stephanypaz <stephanypazb@gmail.com>
2025-04-06 20:58:32 -0300

    subindo para o github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
