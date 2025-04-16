7926c4e67b53b7dbd9907a4532255c8a3dc52ec3
stephanypaz <stephanypazb@gmail.com>
2025-04-16 00:24:03 -0300

    commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardOneComponent } from "./components/small-card-one/small-card-one.component";
 import { SmallCardTwoComponent } from "./components/small-card-two/small-card-two.component";
-import { SmallCardThreeComponent } from "./components/small-card-three/small-card-three.component";
 import { BigCardComponent } from "./components/big-card/big-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SmallCardOneComponent, SmallCardTwoComponent, SmallCardThreeComponent, BigCardComponent, MenuTitleComponent, HomeComponent],
+  imports: [RouterOutlet, SmallCardOneComponent, SmallCardTwoComponent, BigCardComponent, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,11 @@ import { MenuTitleComponent } from "../../components/menu-title/menu-title.compo
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardOneComponent } from "../../components/small-card-one/small-card-one.component";
 import { SmallCardTwoComponent } from "../../components/small-card-two/small-card-two.component";
-import { SmallCardThreeComponent } from "../../components/small-card-three/small-card-three.component";
 import { BigCardTwoComponent } from "../../components/big-card-two/big-card-two.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardOneComponent, SmallCardTwoComponent, SmallCardThreeComponent, BigCardTwoComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardOneComponent, SmallCardTwoComponent, BigCardTwoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
